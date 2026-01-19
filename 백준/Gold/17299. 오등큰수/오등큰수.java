95bd9665b8065dd34c10ed6e7b338b2ee3d1d111
brian506 <135039872+brian506@users.noreply.github.com>
2026-01-19 17:23:44 +0900

    Refactor 오등큰수.java for improved readability

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -1,47 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.*;
-
-
-
+import java.io.*;
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    static class Number {
+        int number;
+        int count;
+        
+        public Number(int number, int count) {
+            this.number = number;
+            this.count = count;
+        }
+    }
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(bf.readLine());
-        int[] data = new int[n];
-        int[] cnt = new int[1_000_001];
-        int[] answer = new int[n];
-
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        for (int i = 0; i < n; i++) {
-            data[i] = Integer.parseInt(st.nextToken());
-            cnt[data[i]]++; // 각 숫자들이 몇개 있는지 cnt[]에 넣는다
+        
+        Map<Integer,Integer> map = new HashMap<>();
+        Stack<Number> stack = new Stack<>();
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        int[] ans = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+            map.put(nums[i], map.getOrDefault(nums[i],0) + 1);
         }
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < n; i++) {
-            /*
-            1.i가 0부터 4까지는 이전에 있던 index들의 count보다 작기 때문에 스택에 그냥 추가한다
-            2. 5번 index가 들어올 때는 5번 index는 2이므로 cnt[2]=2이다. 근데 스택을 peek한 값은 4이다.
-              4번 index의 data는 4이다.count[4] = 1 이므로 조건을 만족시키기 때문에 4번 index를 pop해주고
-              answer[4]의 값을 새로 들어온 index 5의 data 값인 2로 바꿔준다
-            3. 이어서 다시 스택을 peek해보면 index 3이 있다. 조건을 만족하므로 위 과정을 반복한다
-             */
-            while(!stack.isEmpty() && cnt[data[stack.peek()]] < cnt[data[i]]){
-                answer[stack.pop()] = data[i];
+        
+        for(int i = n -1; i >= 0; i--) {
+            int curCount = map.get(nums[i]);
+            
+            while(!stack.isEmpty()) {
+                if(stack.peek().count <= curCount) {
+                    stack.pop();
+                    if(stack.isEmpty()) break;
+                } else {
+                    ans[i] = stack.peek().number;
+                    break;
+                }                
             }
-            stack.add(i);
+            if(stack.isEmpty()) {
+                ans[i] = -1;
+            } 
+            stack.push(new Number(nums[i],curCount));
         }
-        while (!stack.isEmpty())
-            answer[stack.pop()] =-1;
-        //새로 들어오는 data가 없기 때문에 오등큰수를 찾을 수 없어
-        //stack에 남아있는 index들의 answer을 -1로 초기화해준다
-        for (int i = 0; i < n; i++) {
-            sb.append(answer[i]).append(' ');
+        for(int i = 0; i < n; i++) {
+            sb.append(ans[i]).append(" ");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
-
